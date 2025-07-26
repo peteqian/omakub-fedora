588e21c7d1850935e987cb3069d0f45ba22697b6
peteqian <peter.qian.dev@gmail.com>
2025-07-26 16:37:04 +0200

    Version update

--- install/check-version.sh
+++ install/check-version.sh
@@ -8,14 +8,17 @@ fi
 
 . /etc/os-release
 
-# Check if running on Fedora 42 or higher
-if [ "$ID" != "fedora" ] || [ $(echo "$VERSION_ID >= 42" | bc) != 1 ]; then
-  echo "$(tput setaf 1)Error: OS requirement not met"
-  echo "You are currently running: $ID $VERSION_ID"
-  echo "OS required: Fedora 42 or higher"
-  echo "Installation stopped."
-  exit 1
-fi
+# Check if running on Fedora 39 or higher
+# Temporarily disabled for development/testing
+# if [ "$ID" != "fedora" ] || [ $(echo "$VERSION_ID >= 39" | bc) != 1 ]; then
+#   echo "$(tput setaf 1)Error: OS requirement not met"
+#   echo "You are currently running: $ID $VERSION_ID"
+#   echo "OS required: Fedora 39 or higher"
+#   echo "Installation stopped."
+#   exit 1
+# fi
+
+echo "$(tput setaf 3)Warning: Running on $ID $VERSION_ID instead of Fedora - some packages may not be available"
 
 # Check if running on x86
 ARCH=$(uname -m)

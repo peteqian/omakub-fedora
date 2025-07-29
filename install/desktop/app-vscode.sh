634561adb4f11657632b398ee43747327cacd43d
Peter Qian <peter.qian@sciflow.net>
2025-07-29 15:44:30 +0200

    Update

--- install/desktop/app-vscode.sh
+++ install/desktop/app-vscode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 sudo dnf update -y
 sudo dnf install -y code

a7757bf146c402eced9b4baeac3747f7b51dc4be
peteqian <peter.qian.dev@gmail.com>
2025-07-29 12:12:42 +0200

    update gum

--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -3,7 +3,7 @@
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
 cd /tmp
 GUM_VERSION="0.16.2"
-wget -qO gum.rpm "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum-${GUM_VERSION}.x86-64.rpm"
+wget -qO gum.rpm "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum-${GUM_VERSION}-1.x86_64.rpm"
 sudo dnf install -y ./gum.rpm
 rm gum.rpm
 cd -

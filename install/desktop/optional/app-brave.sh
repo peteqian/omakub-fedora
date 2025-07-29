6f6cb3aa0c7961a759280819635f3a8118db2fcf
peterfedoraubunu <peter.qian.dev@gmail.com>
2025-07-29 15:06:06 +0200

    updated command

--- install/desktop/optional/app-brave.sh
+++ install/desktop/optional/app-brave.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf install -y brave-browser
